179847e07128e4b096069f678f5f3b9ac643044a
umuterbas <97316464+umuterbas@users.noreply.github.com>
2022-10-11 13:22:28 -0700

    a

--- migrations/01_appointment.sql
+++ migrations/01_appointment.sql
@@ -1,10 +1,10 @@
-CREATE TABLE appointment 
-    (id SERIAL PRIMARY KEY,
-    time INTEGER,
-    day_id INTEGER REFERENCES day(id)
-    );
 
 CREATE TABLE day
     (id SERIAL PRIMARY KEY,
     name TEXT
+    );
+CREATE TABLE appointment 
+    (id SERIAL PRIMARY KEY,
+    time TEXT,
+    day_id INTEGER REFERENCES day(id)
     );
--- seeds/available_interviewer.sql
+++ seeds/available_interviewer.sql
@@ -1,2 +1,2 @@
-INSERT INTO available_interviewer (id, interviewer_id, day_id) VALUES (1, 1, 1); --change day_id to be available more days
-INSERT INTO available_interviewer (id, interviewer_id, day_id) VALUES (1, 1, 2);
+-- INSERT INTO available_interviewer (id, interviewer_id, day_id) VALUES (1, 1, 1); --change day_id to be available more days
+INSERT INTO available_interviewer (id, interviewer_id, day_id) VALUES (2, 1, 2);
