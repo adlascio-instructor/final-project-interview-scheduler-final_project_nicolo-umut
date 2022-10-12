b257265e20c2027b46e25a8ce8425ac5843563c8
DarthStarLorD77 <97316470+DarthStarLorD77@users.noreply.github.com>
2022-10-12 12:25:38 -0700

    second commit
    base for first part
    added socket.io (still basic)
    fixed some errors

--- seeds/day_seeds.sql
+++ seeds/day_seeds.sql
@@ -1,5 +1,5 @@
-INSERT INTO day_id (id, name) VALUES (1, 'MONDAY');
-INSERT INTO day_id (id, name) VALUES (2, 'TUESDAY');
-INSERT INTO day_id (id, name) VALUES (3, 'WEDNESDAY');
-INSERT INTO day_id (id, name) VALUES (4, 'THURSDAY');
-INSERT INTO day_id (id, name) VALUES (5, 'FRIDAY');
+INSERT INTO day (id, name) VALUES (1, 'MONDAY');
+INSERT INTO day (id, name) VALUES (2, 'TUESDAY');
+INSERT INTO day (id, name) VALUES (3, 'WEDNESDAY');
+INSERT INTO day (id, name) VALUES (4, 'THURSDAY');
+INSERT INTO day (id, name) VALUES (5, 'FRIDAY');
--- server.js
+++ server.js
@@ -1,5 +1,86 @@
 const express = require("express");
 const app = express();
+const http = require("http");
+const socketIO = require("socket.io");
+const { Pool } = require("pg");
 const port = 8000;
 
+const server = http.createServer(app);
+const io = socketIO(server);
+
+io.on("connection", (socket) => {
+   const { username, room } = socket.handshake.query;
+   console.log("A client has connected", username);
+   socket.join(room);
+   io.to(room).emit("welcome_message", {
+     username: "Chat Bot",
+     text: `${username} joined`,
+     time: moment().format("hh:mm a"),
+   });
+ 
+   socket.on("message", (message) => {
+     console.log("message in server", message);
+     io.to(room).emit("message", message);
+   });
+ 
+   socket.on("disconnect", () => {
+     console.log("A client has disconnected");
+     io.to(room).emit("message", {
+       username: "Chat Bot",
+       text: `${username} disconnected`,
+     });
+   });
+ });
+
+app.get("/interview", (req, res) => {
+    const { appointment_id } = req.body;
+    const pool = new Pool ({
+       name: process.env.DB_NAME, //username postgres
+       host: process.env.DB_HOST, // localhost
+       database: process.env.DB_DATABASE, // database name
+       password: process.env.DB_PASSWORD, // password username
+       port: process.env.DB_PORT, // 5432 
+    });
+    pool   
+       .query("SELECT appointment_id FROM interview")
+       .then((result) => result.rows)
+       .then((interview) => res.json(interview))
+       .catch((err) => console.log("err"))
+       .finally(() => pool.end());        
+});
+
+app.get("/interview", (req, res) => {
+    const pool = new Pool ({
+       name: process.env.DB_NAME, //username postgres
+       host: process.env.DB_HOST, // localhost
+       database: process.env.DB_DATABASE, // database name
+       password: process.env.DB_PASSWORD, // password username
+       port: process.env.DB_PORT, // 5432 
+    });
+    pool   
+       .query("SELECT * FROM interviewer")
+       .then((result) => result.rows)
+       .then((interviewer) => res.json(interviewer))
+       .catch((err) => console.log("err"))
+       .finally(() => pool.end());        
+});
+
+app.get("/available_interviewer", (req, res) => {
+    const pool = new Pool ({
+       name: process.env.DB_NAME, //username postgres
+       host: process.env.DB_HOST, // localhost
+       database: process.env.DB_DATABASE, // database name
+       password: process.env.DB_PASSWORD, // password username
+       port: process.env.DB_PORT, // 5432 
+    });
+    pool   
+       .query("SELECT day_id FROM available_interviewer")
+       .then((result) => result.rows)
+       .then((day_id) => res.json(day_id))
+       .catch((err) => console.log("err"))
+       .finally(() => pool.end());        
+});
+
 app.listen(port, () => console.log(`Server is running on port ${port}`));
+
+
