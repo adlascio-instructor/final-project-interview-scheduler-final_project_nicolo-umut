05f4c399f84d08d7bce04db33dec01f67f4375c7
DarthStarLorD77 <97316470+DarthStarLorD77@users.noreply.github.com>
2022-10-14 12:13:22 -0700

    working on get from client to server

--- server.js
+++ server.js
@@ -82,9 +82,8 @@ app.get("/interview/:day", (req, res) => {
        .catch((err) => console.log("err", err))
        .finally(() => pool.end());        
 });
-// loop and create object from array, if the array doesnt have anything just pass empty data if its filled it should pass every data
-
-app.get("/interviewer", (req, res) => {
+        
+app.get("/available_interviewer/:day", (req, res) => {
     const pool = new Pool ({
        name: process.env.DB_NAME, //username postgres
        host: process.env.DB_HOST, // localhost
@@ -93,29 +92,38 @@ app.get("/interviewer", (req, res) => {
        port: process.env.DB_PORT, // 5432 
     });
     pool   
-       .query("SELECT * FROM interviewer")
+       .query(`SELECT interviewer.id AS interviewer_id, interviewer.name AS interviewer_name, interviewer.avatar AS interviewer_avatar FROM available_interviewer
+       JOIN day
+       ON available_interviewer.day_id = day.id
+       LEFT JOIN interviewer
+       ON available_interviewer.interviewer_id = interviewer.id
+       WHERE day.name = '${req.params.day}'
+       ORDER BY available_interviewer.id;`)
        .then((result) => result.rows)
-       .then((interviewer) => res.json(interviewer))
-       .catch((err) => console.log("err"))
-       .finally(() => pool.end());        
+       .then((availableInterviewer) => {
+         // console.log(schedule);
+
+         // const availableInterviewObj = {};
+
+         // availableInterviewer.forEach((element) => {
+         //    availableInterviewObj[element.available_interviewer_id] = {
+         //       id: element.available_interviewer_id,
+         //       time: element.time,
+         //    }
+         //    if(element.interviewer) {
+         //       availableInterviewObj[element.available_interviewer_id].interviewer = {
+         //             id: element.interviewer_id,
+         //             name: element.interviewer_name,
+         //             avatar: element.interviewer_avatar
+         //          }
+         //       } 
+         // })
+         res.json(availableInterviewer);
+      })
+       .catch((err) => console.log("err", err))
+       .finally(() => pool.end());          
 });
 
-app.get("/available_interviewer", (req, res) => {
-    const pool = new Pool ({
-       name: process.env.DB_NAME, //username postgres
-       host: process.env.DB_HOST, // localhost
-       database: process.env.DB_DATABASE, // database name
-       password: process.env.DB_PASSWORD, // password username
-       port: process.env.DB_PORT, // 5432 
-    });
-    pool   
-       .query("SELECT day_id FROM available_interviewer")
-       .then((result) => result.rows)
-       .then((day_id) => res.json(day_id))
-       .catch((err) => console.log("err"))
-       .finally(() => pool.end());        
-});
+//make request from the client to the server
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
-
-
--- test.sql
+++ test.sql
@@ -12,13 +12,21 @@
 -- WHERE name = 'MONDAY'
 -- ORDER BY appointment.id;
 
-SELECT appointment.id AS appointment_id, appointment.time, interview.student,
-interviewer.id AS interviewer_id, interviewer.name AS interviewer_name, interviewer.avatar AS interviewer_avatar FROM appointment
+-- SELECT appointment.id AS appointment_id, appointment.time, interview.student,
+-- interviewer.id AS interviewer_id, interviewer.name AS interviewer_name, interviewer.avatar AS interviewer_avatar FROM appointment
+-- JOIN day
+-- ON appointment.day_id = day.id
+-- LEFT JOIN interview
+-- ON appointment.id = interview.appointment_id
+-- LEFT JOIN interviewer
+-- ON interview.interviewer_id = interviewer.id
+-- WHERE day.name = 'MONDAY'
+-- ORDER BY appointment.id;
+
+SELECT available_interviewer.id AS available_interviewer_id, interviewer.id AS interviewer_id, interviewer.name AS interviewer_name, interviewer.avatar AS interviewer_avatar FROM available_interviewer
 JOIN day
-ON appointment.day_id = day.id
-LEFT JOIN interview
-ON appointment.id = interview.appointment_id
+ON available_interviewer.day_id = day.id
 LEFT JOIN interviewer
-ON interview.interviewer_id = interviewer.id
-WHERE day.name = 'MONDAY'
-ORDER BY appointment.id;
+ON available_interviewer.interviewer_id = interviewer.id
+WHERE day.name = 'WEDNESDAY'
+ORDER BY available_interviewer.id;
