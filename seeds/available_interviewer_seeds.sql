87de81dc5cb50bbbbdfe8ae95fde96246c49922a
umuterbas <97316464+umuterbas@users.noreply.github.com>
2022-11-17 10:24:55 -0800

    a

--- seeds/available_interviewer_seeds.sql
+++ seeds/available_interviewer_seeds.sql
@@ -1,4 +1,4 @@
-INSERT INTO available_interviewer (id, interviewer_id, day_id) VALUES (1, 1, 1); --change day_id to be available more days
+INSERT INTO available_interviewer (id, interviewer_id, day_id) VALUES (1, 1, 1); 
 INSERT INTO available_interviewer (id, interviewer_id, day_id) VALUES (2, 2, 3);
 INSERT INTO available_interviewer (id, interviewer_id, day_id) VALUES (3, 3, 1); 
 INSERT INTO available_interviewer (id, interviewer_id, day_id) VALUES (4, 3, 4);
